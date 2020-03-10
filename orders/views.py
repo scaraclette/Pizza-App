24cb2e6854c17137d49115b9274b138e675844aa
Scarlett Halima <scarlett.halima@pacificoffice.com>
2020-03-10 13:38:51 -0700

    added menu page

--- orders/views.py
+++ orders/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def menu(request):
+    return render(request, "menu.html")
