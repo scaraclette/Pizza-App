1a3ccb4923207ea06266696c81a8f659e446c683
scaraclette <gustiscarlett@gmail.com>
2020-03-19 11:11:06 -0700

    final

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('newGoal')
+            return redirect('index')
 
     else:
         form = UserCreationForm()
